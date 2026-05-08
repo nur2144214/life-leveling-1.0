83e3ef7a7e2ffaadd2bba00c77dd4b454be836f1
nur2144214 <nur2144214@gmail.com>
2026-05-08 19:01:30 +0600

    .env fix

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -77,7 +77,7 @@ export default function App() {
  
   const handleSignIn = async () => {
     try {
-      const res = await fetch('http://localhost:8000/api/login/', {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/login/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({}),
